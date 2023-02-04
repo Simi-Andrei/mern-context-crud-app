ebbc2ca70b9b3c82909ba4809d0680357ee672ef
SimigiuAndrei <andy_andrei9572@yahoo.com>
2023-02-04 18:51:19 +0200

    deployment

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,13 @@ app.use("/api/products", productRoutes);
 
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
